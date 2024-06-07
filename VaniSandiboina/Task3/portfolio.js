cb588b83f072fbd1872cfbb038e353a00675834b
VaniSandiboina <vanisandiboina123@gmail.com>
2024-06-07 15:56:03 +0530

    Completed Task 3

---
+++ VaniSandiboina/Task3/portfolio.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$(".nav-link").on('click', function(event) {
+
+    	if (this.hash !== "") {
+
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 700, function(){
+				window.location.hash = hash;
+			});
+      	} 
+    });
+});
